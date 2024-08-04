12ac776d3187c7ce1a5b5e527a6bebb196e9b5cb
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-04 07:34:27 -0400

    importing to auth in routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
