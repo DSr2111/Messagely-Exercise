64e31957e423eb8efb635684463bb978fe992d93
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-03 21:43:27 -0400

    get list of users and get detail of a user done

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,36 @@ const User = require("../models/user");
 
 const router = new Router();
 
-router.get("/", async function (req, res, next) {
-  try {
-  } catch (err) {
-    return next(err);
-  }
-});
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get("/", async function (req, res, next) {
+  try {
+    let users = await User.all();
+
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username", async function (req, res, next) {
+  try {
+    let user = await User.get(req.params.username);
+
+    return res.json({ user });
+  } catch (error) {
+    return next(err);
+  }
+});
 
 /** GET /:username/to - get messages to user
  *
